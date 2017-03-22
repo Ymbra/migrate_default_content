7630cfb98d2498160c74e0c9c8f4123a4bb7c3f3
Ramon Vilar <ramon@ymbra.com>
2017-03-22 19:51:10 +0100

    Created initial structure for the data source plugin manager.

---
+++ src/MigrateDefaultContentSourcePluginManager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\migrate_default_content;
+
+use Drupal\Core\Plugin\DefaultPluginManager;
+
+/**
+ * Provides a source plugin manager for data source for migrations.
+ */
+class MigrateDefaultContentSourcePluginManager extends DefaultPluginManager {
+
+}
