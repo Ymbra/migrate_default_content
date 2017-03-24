142ad5d85bdec588ccfe00bb3901ebef67725014
Ramon Vilar <ramon@ymbra.com>
2017-03-24 19:45:00 +0100

    Added Plugin manager Interface and Annotation

---
+++ src/Plugin/MigrateDefaultContent/Source/Yaml.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\migrate_default_content\Plugin\MigrateDefaultContent;
+
+use Drupal\migrate_default_content\SourcePluginInterface;
+
+/**
+ * Defines a Yaml data source implementation.
+ *
+ * @Source(
+ *   id = "Yaml",
+ *   extension = "yml"
+ * )
+ */
+class Yaml implements SourcePluginInterface {
+}
---
+++ src/SourcePluginInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\migrate_default_content;
+
+/**
+ * Defines the common interface for all SourcePlugin classes.
+ */
+interface SourcePluginInterface {
+
+}
