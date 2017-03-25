70df64d4452ae9334dbddc781e5bf9cdfccb66c3
Ramon Vilar <ramon@ymbra.com>
2017-03-25 13:35:18 +0100

    Finished the plugin system for source data and changed the logic of the module to use it.

--- src/Plugin/MigrateDefaultContent/Source/Yaml.php
+++ src/Plugin/MigrateDefaultContent/Source/Yaml.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Drupal\migrate_default_content\Plugin\MigrateDefaultContent;
+namespace Drupal\migrate_default_content\Plugin\MigrateDefaultContent\Source;
 
-use Drupal\migrate_default_content\SourcePluginInterface;
+use Symfony\Component\Yaml\Parser;
+use Drupal\migrate_default_content\BaseSourcePlugin;
 
 /**
  * Defines a Yaml data source implementation.
@@ -12,5 +13,40 @@ use Drupal\migrate_default_content\SourcePluginInterface;
  *   extension = "yml"
  * )
  */
-class Yaml implements SourcePluginInterface {
+class Yaml extends BaseSourcePlugin {
+
+  /**
+   * Yaml constructor.
+   *
+   * @param array $configuration
+   *   Entire configuration for data source plugin.
+   */
+  public function __construct(array $configuration) {
+    parent::__construct($configuration);
+
+    // Initialize header.
+    $parser = new Parser();
+    $data = $parser->parse(file_get_contents($this->getFullPathFile()));
+    $this->header = array_keys($data[0]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSourceMigrationDefinition() {
+    return [
+      'plugin' => 'yaml',
+      'file' => $this->getFullPathFile(),
+      'ids' => [
+        $this->getKey() => [
+          'type' => 'string',
+        ],
+      ],
+      'fields' => array_combine($this->getHeader(), $this->getHeader()),
+      'constants' => [
+        'langcode' => $this->getLanguage(),
+      ],
+    ];
+  }
+
 }
